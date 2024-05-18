4fc3621172a7a21d1e985b41352a366f3e37af17
Sean Springer <ss28@pdx.edu>
2024-05-18 10:21:49 -0700

    finished 07 - JS

--- 07-javascript/01-replace.js
+++ 07-javascript/01-replace.js
@@ -4,12 +4,17 @@ const replaceItems = (input) => {
   // for reference, see the MDN "Regular Expressions" entry:
   // https://developer.mozilla.org/en-US/docs/Web/JavaScript/Guide/Regular_Expressions
 
-  return input;
+  //g stands for global which is necessary
+  //i is for case incensitive
+  const reg1 = /gold/gi;
+  const reg2 = /wander/gi;
+
+  return input.replaceAll(reg1, "SHINY").replaceAll(reg2, "room");
 };
 
-console.log(replaceItems('All that is gold does not glitter'));
+console.log(replaceItems("All that is gold does not glitter"));
 // expected: 'All that is SHINY does not glitter'
-console.log(replaceItems('Wisdom is better than silver or GOLD.'));
+console.log(replaceItems("Wisdom is better than silver or GOLD."));
 // expected: 'Wisdom is better than silver or SHINY.'
-console.log(replaceItems('Not all those who Wander are lost'));
+console.log(replaceItems("Not all those who Wander are lost"));
 // expected: 'Not all those who roam are lost'
--- 07-javascript/04-dom.js
+++ 07-javascript/04-dom.js
@@ -2,16 +2,32 @@
 // when the user clicks the 'copy' button, copy the user input to the output area
 
 // fetch JavaScript objects representing specific elements in the DOM
+let userInput = document.getElementById("userInput1");
+let copy = document.querySelector("#copy");
+let output = document.querySelector(".output");
 
 // add an event listener on the target element
+copy.addEventListener("click", handleClick);
 
 // callback function to handle event
+function handleClick(event) {
+  console.log(userInput.value);
+  output.textContent = userInput.value;
+}
 
 // Exercise #2:
 // when the user enters input text, copy the user input to the output area
-
 // fetch JavaScript objects representing specific elements in the DOM
-
 // add an event listener on the target element
-
 // callback function to handle event
+
+const input2 = document.getElementById("userInput2");
+const outerDiv = document.getElementById("inputEventExample");
+const output2 = document.createElement("div");
+output2.classList.add("output");
+
+outerDiv.append(output2);
+
+input2.addEventListener("input", (event) => {
+  output2.textContent = input2.value;
+});
